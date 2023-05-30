f5915f6e3202749b2a3cbf301199cab6ef758340
Suzy@DESKTOP <suzynh4gyn@gmail..com>
2023-05-30 19:43:59 -0300

    desafio finalizado

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function criptografar(){
+function criptografar() {
     const textoInicial = utils()
     const novoTexto = document.getElementById("novoTexto")
-     const arrayListText = textoInicial.split('');
-     
-     const arrayListCript = []
-    
-textoInicial.split()
-    for (let i = 0; i <arrayListText.length; i++){
-        if(arrayListText[i] == 'a') {
+    const arrayListText = textoInicial.split('');
+
+    const arrayListCript = []
+
+    textoInicial.split()
+    for (let i = 0; i < arrayListText.length; i++) {
+        if (arrayListText[i] == 'a') {
             arrayListCript.push('ai')
         } else if (arrayListText[i] == 'e') {
-            arrayListCript.push('enter') 
+            arrayListCript.push('enter')
         } else if (arrayListText[i] == 'i') {
             arrayListCript.push('imes')
         } else if (arrayListText[i] == 'o') {
@@ -21,44 +21,43 @@ textoInicial.split()
             arrayListCript.push(arrayListText[i])
         }
     }
-  
+
 
     novoTexto.innerHTML = arrayListCript.join('')
     console.log(novoTexto)
-    
+
 }
 
-function descriptografar(){
+function descriptografar() {
     const textoFinal = utils()
-    const novoTexto = document. getElementById("novoTexto")
+    const novoTexto = document.getElementById("novoTexto")
     const descriptografar = textoFinal.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a')
-    .replaceAll('ober', 'o').replaceAll('ufat', 'u')
+        .replaceAll('ober', 'o').replaceAll('ufat', 'u')
 
 
     novoTexto.innerHTML = descriptografar
-    
+
 }
 
 function utils() {
     const textoInicial = document.getElementById("texto").value.toLowerCase()
-   
+
     const resultado = document.getElementById('resultado')
     const mensagem = document.getElementById('mensagem')
     const apagar = document.getElementById("apagar")
     const novoTextoPai = document.getElementById('novoTextoPai')
 
-    
-    resultado.style.justifyContent = 'start' 
+
+    resultado.style.justifyContent = 'start'
     novoTextoPai.classList.add("aparecer")
 
     // mensagem.style.display = 'none'
     // apagar.style.display = 'none'
     // resultado.style.justifyContent = 'start' 
     // novoTextoPai.classList.add("aparecer")
-    
+
 
     return textoInicial;
-    
-} 
 
-    
+}
+
